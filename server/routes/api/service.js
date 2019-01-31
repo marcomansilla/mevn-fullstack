e212246439e4aea687de742a18bfdb200164b7de
marco <marco@xignus.com.ar>
2019-01-31 01:48:31 -0300

    trabajando en la api del servidor y sincronizando el cliente

--- server/routes/api/service.js
+++ server/routes/api/service.js
@@ -3,7 +3,6 @@ const mongodb = require('mongodb');
 
 var router = express();
 
-
 // improve this function
 router.get('/search',async (req, res)=>{
     res.send(req.query[Object.keys(req.query)[0]]);
@@ -16,23 +15,11 @@ router.get('/:collection',async (req, res)=>{
     res.send(await collections.collection(req.params.collection).find().toArray());
 });
 
-
-// Add collections
-// router.post('/:collection',async (req, res)=>{
-//     const collections = await loadCollections();
-//     await collections.collection(req.params.collection).insertOne({
-//	text:req.body.text,
-//	createdAt:new Date()
-//     });
-//     res.status(201).send();
-// });
-
 router.post('/:collection',async (req, res)=>{
     const collections = await loadCollections();
     await collections.collection(req.params.collection).insertOne(req.body);
     res.status(201).send();
 });
-
 
 // Delete posts
 router.delete('/:collection/:id', async (req, res)=>{
@@ -40,6 +27,7 @@ router.delete('/:collection/:id', async (req, res)=>{
     await collections.collection(req.params.collection).deleteOne({_id:mongodb.ObjectID(req.params.id)});
     res.status(204).send();
 });
+
 // Update posts
 router.put('/:collection/:id', async (req, res)=>{
     const collections=await loadCollections();
@@ -51,7 +39,6 @@ router.put('/:collection/:id', async (req, res)=>{
     );
     res.status(201).send();
 });
-
 
 // Database api code
 var loadCollections = async ()=>{
