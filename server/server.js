a4d6c9df76d200e4ab9b4db95075a5e1df177bfe
marco <marco@xignus.com.ar>
2019-02-09 12:19:57 -0300

    fixed path bug and api blocking urls

--- server/server.js
+++ server/server.js
@@ -10,23 +10,19 @@ const PORT = process.env.port || 3000;
 
 var app = express();
 
-if (process.env.NODE_ENV==='production'){
-    // Set static folder
-    app.use(express.static(__dirname + '/public/'));
-
-    // Handle SPA
-    app.get('/', (req, res)=>res.sendFile(__dirname+'/public/index.html'));
-}
-
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/api', api);
 
-app.get("/", (req, res)=>{
-    res.send("");
-});
+if (process.env.NODE_ENV==='production'){
+    // Set static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res)=>res.sendFile(__dirname+'/public/index.html'));
+}
 
 app.listen(PORT,()=>{
     console.log(`Server running on port: ${PORT}`);
