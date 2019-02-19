954803a27b6027b8655651cf1c28bff8536dc885
marco <marco@xignus.com.ar>
2019-02-19 15:34:23 -0300

    end

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,24 +1,24 @@
 <template>
-<div class="container">
-  <h1>Biblioteca MEVN - App de prueba</h1>
+    <div class="container">
+	<h1>Biblioteca MEVN - App de prueba</h1>
 
-  <Listado />
-  <footer>
-    <p>Copyleft 2018</p>
-  </footer>
-</div>
+	<Listado />
+	<footer>
+	    <p>Copyleft 2018</p>
+	</footer>
+    </div>
 </template>
 
 <script>
-import Listado from './components/listado.vue'
-export default {
-    components:{
-	Listado
-    },
-    data(){
-	return {}
-    }
-}
+ import Listado from './components/listado.vue'
+ export default {
+     components:{
+	 Listado
+     },
+     data(){
+	 return {}
+     }
+ }
 </script>
 
 <style>
